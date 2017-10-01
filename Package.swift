e388af2ab4b3821049ac443ae700d9504024a748
Alexandre Simoes Tavares <alexandre.simoes@etu.unige.ch>
2017-10-01 21:31:12 +0200

    Specify tools version to allow compilation with swift 4
    See https://swift.org/blog/swift-package-manager-manifest-api-redesign/

--- Package.swift
+++ Package.swift
@@ -1,7 +1,17 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-
 let package = Package(
-    name: "PetriKit")
+    name: "MyApp",
+    dependencies: [
+        .package(url: "https://github.com/kyouko-taiga/PetriKit.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "MyApp",
+            dependencies: ["PetriKit"]
+        )
+    ]
+)
